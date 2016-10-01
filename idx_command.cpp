34b11b5c527e2ff56c67a33eea87d48bdc53c6fe
Eric Busboom <eric@clarinova.com>
2016-09-30 22:42:47 -0700

    .

--- idx_command.cpp
+++ idx_command.cpp
@@ -3,6 +3,80 @@
 
 #include "idx_command.h"
 #include <Arduino.h>
+#include "CRC32.h"
+
+
+
+
+// Wait for a header sync string, then read the entire header. 
+int IDXCommandBuffer::run(){
+    
+    if(ser.available()){
+        
+        char c =  ser.read();
+        
+        if (buf_pos < sizeof(last_command->sync)) {
+           
+            // When reading the sync string, ensure that each additional char in 
+            // the buffer equals the one in the same position in the sync str. 
+            // If not, the characters read so far aren't sync chars, so start over. 
+            if ( c == last_command->sync[buf_pos] ){
+                buf_pos++;
+                
+            } else{
+                buf_pos = 0;
+            }
+            Serial.print(c);
+       
+        } else {
+            
+            *(char*)(((char*)last_command)+buf_pos) = c;
+            
+            buf_pos++;
+            
+            //Serial.print(buf_pos-1);Serial.print(" ");
+            //Serial.print((uint8_t)c);Serial.print(" ");
+            //Serial.println(*(uint8_t*)(((uint8_t*)last_command)+buf_pos-1) );
+            
+            if (buf_pos == sizeof(struct command)){
+                
+                uint32_t crc  = CRC32::checksum( (const uint8_t*)last_command, 
+                                             sizeof(*last_command) - sizeof(last_command->crc));
+                 
+                
+                Serial.println(crc);
+                if (crc == last_command->crc){
+                    Serial.println("ACK");
+                    commands.add(last_command);
+                    sendResponse(*last_command, IDX_COMMAND_ACK );
+                    last_command = new command();
+                } else {
+                    Serial.println("nack");
+                    sendResponse(*last_command, IDX_COMMAND_NACK );
+                }
+           
+                buf_pos = 0;
+            }
+        }
+    }
+    
+    return buf_pos;
+}
+
+void IDXCommandBuffer::send(struct command & command){     
+    
+    uint32_t crc  = CRC32::checksum( (const uint8_t*)&command, 
+                                 sizeof(*last_command) - sizeof(last_command->crc));
+          
+    ser.write( (uint8_t*)&command, sizeof(struct command));
+}
+
+void IDXCommandBuffer::sendResponse(struct command & command, int code){ 
+    response.seq = command.seq;
+    response.code = code;         
+    send(response);
+}
+
 
 
 // From Wikipedia: https://en.wikipedia.org/wiki/Fletcher%27s_checksum
@@ -26,4 +100,33 @@ uint16_t fletcher16( uint8_t const *data, size_t bytes )
     return sum2 << 8 | sum1;
 }
 
+/*
+# Nasty hack based on 
+# Wikipedia: https://en.wikipedia.org/wiki/Fletcher%27s_checksum
+def fletcher16( data ):
 
+    sum1 = 0xff
+    sum2 = 0xff
+    tlen = 0
+    i = 0
+    bytes = len(data)
+    
+    while (bytes):
+        tlen = 20 if bytes >= 20 else bytes
+        bytes -= tlen
+        while True:
+            sum1 += ord(data[i])
+            sum2 += sum1
+            i += 1
+            tlen -= 1
+            if tlen == 0:
+                break
+     
+        sum1 = (sum1 & 0xff) + (sum1 >> 8)
+        sum2 = (sum2 & 0xff) + (sum2 >> 8)
+    
+ 
+    sum1 = (sum1 & 0xff) + (sum1 >> 8)
+    sum2 = (sum2 & 0xff) + (sum2 >> 8)
+    return sum2 << 8 | sum1;
+*/
--- idx_command.h
+++ idx_command.h
@@ -9,120 +9,86 @@
 #define IDX_COMMAND_NACK 0  // Failed to read payload
 #define IDX_COMMAND_ACK 1   // Payload successfully stored in meggage list
 #define IDX_COMMAND_DONE 2  // COmmand completed
-#define IDX_COMMAND_POSITION 10
-#define IDX_COMMAND_VELOCITY 11
-#define IDX_COMMAND_ACCELERATION 12
+#define IDX_COMMAND_APOSITION 10
+#define IDX_COMMAND_RPOSITION 11
+#define IDX_COMMAND_VELOCITY 12
+#define IDX_COMMAND_ACCELERATION 13
 #define IDX_COMMAND_POSITIONQUERY 20
+
+
+#define IDX_READING_HEADER 1
+#define IDX_READING_BODY 2 
 
 // A Header packet that preceedes all commands, and also serves as
 // ACK and NACK
 
-struct command_header {
+
+struct command {
     byte sync[4] = {'I','D','X','C'};
     uint16_t code; // command code
-    uint16_t length; // payload length
-    int32_t seq; // Packet sequence
-    int32_t crc; // Payload CRC
-    int32_t checksum; // Checksum for header. Actaull a uint16_t, but gets padded to 32b anyway. 
-};
-
-struct acceleration_command {
-    float t;
-    float accelerations[6] = {NAN,NAN,NAN,NAN,NAN,NAN}; 
-    int32_t positions[6] = {LONG_MIN,LONG_MIN,LONG_MIN,LONG_MIN,LONG_MIN,LONG_MIN}; 
-};
-
-struct velocity_command {
-    float t;
+    uint16_t seq; // Packet sequence
+    int32_t directions; // Direction bits
     float velocities[6] = {NAN,NAN,NAN,NAN,NAN,NAN}; 
-    int32_t positions[6] = {LONG_MIN,LONG_MIN,LONG_MIN,LONG_MIN,LONG_MIN,LONG_MIN}; 
+    int32_t positions[6] = {0,0,0,0,0,0}; 
+    uint32_t crc; // Payload CRC
 };
 
-struct position_command {
-    int32_t positions[6] = {LONG_MIN,LONG_MIN,LONG_MIN,LONG_MIN,LONG_MIN,LONG_MIN}; 
-};
-
-struct message {
-    struct command_header *header;
-    void* command;
-    void* scratch;
-};
 
 uint16_t fletcher16( uint8_t const *data, size_t bytes );
 
 class IDXCommandBuffer {
 
 private:
+
+    LinkedList<struct command*> commands;
     
-    LinkedList<struct message*> messages;
+    struct command *last_command;
     
-    // Buffer to load incomming command headers
-    char header_buf[sizeof(struct command_header)];
-    int header_buf_pos = 0;
-    
+    int buf_pos = 0;
+   
     Serial_ &ser; // SerialUSB on the Arduino Due
     
-    struct command_header response;
+    struct command response; 
+    
     
 public:
     
     IDXCommandBuffer(Serial_ &ser) : ser(ser) {
-        
+        last_command =  new command();
+       
     }
 
-    inline void sendHeader(struct command_header & header){
-              
-        header.checksum = fletcher16( (uint8_t*)&header, 
-                                    sizeof(struct command_header)-sizeof(int32_t) );
-                               
-        ser.write( (uint8_t*)&header, sizeof(struct command_header));
+    void send(struct command & command);
+    
+    void sendResponse(struct command & command, int code);
+    
+    // Wait for a header sync string, then read the entire header. 
+    int run();
+
+    inline int size(){
+        return commands.size();
     }
-
-    int runForHeader(){
-        
-        while(ser.available()){
-          header_buf[header_buf_pos++] =  ser.read();
-
-          if (header_buf_pos == sizeof(struct command_header)){
-            struct message *msg = new message();
-            msg->header =  new command_header();
-            memcpy(msg->header, header_buf, sizeof(struct command_header));
-            memcpy(&response, header_buf, sizeof(struct command_header));
-            header_buf_pos = 0;
-            messages.add(msg);
-            
-            response.code = IDX_COMMAND_ACK;
-            response.length = 0;
-
-            sendHeader(response);
-            
-          } 
+    
+    inline int buflen(){
+        return buf_pos;
+    }
+    
+    
+    inline void removeMessage(){
+        if ( size() == 0){
+            return;
         }
         
-        return header_buf_pos;
-    }
-
-    
-    // After a header is loaded, load a payload. 
-    void runForPayload(){
+        delete commands.shift();
         
     }
     
-    inline int size(){
-        return messages.size();
-    }
-    
-    inline void freeMsg(struct message * msg){
-        delete msg->header;
-        delete msg;
-    }
-    
-    inline struct message * nextMessage(){
+    inline struct command * getMessage(){
         if ( size() == 0){
             return 0;
         }
         
-        return messages.pop();
+        commands.get(0);
         
     }
 
