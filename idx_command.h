539e42994c34e7ad4bab2b2bc8022456539846c5
Eric Busboom <eric@clarinova.com>
2016-10-01 22:08:34 -0700

    .

--- idx_command.h
+++ idx_command.h
@@ -8,7 +8,7 @@
 #include <LinkedList.h>  // https://github.com/ivanseidel/LinkedList
 #define IDX_COMMAND_NACK 0  // Failed to read payload
 #define IDX_COMMAND_ACK 1   // Payload successfully stored in meggage list
-#define IDX_COMMAND_DONE 2  // COmmand completed
+#define IDX_COMMAND_DONE 2  // Command completed
 #define IDX_COMMAND_APOSITION 10
 #define IDX_COMMAND_RPOSITION 11
 #define IDX_COMMAND_VELOCITY 12
@@ -24,15 +24,30 @@
 
 
 struct command {
-    byte sync[4] = {'I','D','X','C'};
-    uint16_t code; // command code
-    uint16_t seq; // Packet sequence
-    int32_t directions; // Direction bits
-    float velocities[6] = {NAN,NAN,NAN,NAN,NAN,NAN}; 
-    int32_t positions[6] = {0,0,0,0,0,0}; 
-    uint32_t crc; // Payload CRC
-};
+    byte sync[4] = {'I','D','X','C'}; // 4
+    uint16_t code = 0; // command code // 2
+    uint16_t seq = 0; // Packet sequence //2
+    int32_t directions =0 ; // Direction bits // 4
+    uint16_t ticks[6] = {0,0,0,0,0,0}; // 12
+    uint16_t steps[6] = {0,0,0,0,0,0};  // 12
+    uint32_t crc = 0; // Payload CRC // 4
+}; // 40
 
+/*
+Total 64b
+uint16 code
+uint16 seq
+union { 
+    uint32[56] commands;
+    struct {
+        int32_t pad; //4
+        int32_t directions =0 ; // Direction bits // 4
+        float velocities[6] = {NAN,NAN,NAN,NAN,NAN,NAN}; // 24
+        int32_t positions[6] = {0,0,0,0,0,0};  // 24
+    }
+}
+uint32 crc
+*/
 
 uint16_t fletcher16( uint8_t const *data, size_t bytes );
 
@@ -56,6 +71,7 @@ public:
     IDXCommandBuffer(Serial_ &ser) : ser(ser) {
         last_command =  new command();
        
+       
     }
 
     void send(struct command & command);
@@ -73,24 +89,15 @@ public:
         return buf_pos;
     }
     
-    
-    inline void removeMessage(){
-        if ( size() == 0){
-            return;
-        }
-        
-        delete commands.shift();
-        
-    }
-    
     inline struct command * getMessage(){
         if ( size() == 0){
-            return 0;
+            return 0 ;
         }
         
-        commands.get(0);
+        return commands.shift();
         
     }
+    
 
     
 };
